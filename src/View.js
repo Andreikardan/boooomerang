28e796d0219f1a5a3f467c03506c218af8f6c64a
Andreikardan <timoshenko.andrei_1998@mail.ru>
2024-11-01 12:40:25 +0300

    isRunning

--- src/View.js
+++ src/View.js
@@ -1,12 +1,16 @@
 // Сделаем отдельный класс для отображения игры в консоли.
 
+// const runInteractiveConsole = require("./keyboard");
+
 class View {
-  render() {
+  render(track) {
     const yourTeamName = 'Elbrus';
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(''));
+    // runInteractiveConsole()
+
+    console.log(track.join(''));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
